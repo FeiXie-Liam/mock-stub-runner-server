3a8f6d5fbd168ea7795cc4236c98474148de72fe
feixie-liam <xiefei0887@163.com>
2018-08-30 21:34:14 +0800

    [Fei XIE] : complete mock-server demo

--- src/main/java/com/thoughtworks/contract/ContractApplication.java
+++ src/main/java/com/thoughtworks/contract/ContractApplication.java
@@ -2,8 +2,12 @@ package com.thoughtworks.contract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@AutoConfigureStubRunner
 public class ContractApplication {
 
     public static void main(String[] args) {
